ca95e666f96be9bc08703c50a1525544997dea99
BASHAR-eng10 <ba.ad.sy.2003@gmail.com>
2025-08-12 23:33:33 +0300

    example

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import { Shield, Mail, ArrowRight, CheckCircle, AlertCircle, Loader2, Lock, Eye, EyeOff } from 'lucide-react'
 import apiService from "../../services/api"
 // jnud
+//kjdoidj
 interface Account {
   password: string
   role: string
